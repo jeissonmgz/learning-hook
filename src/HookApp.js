f1a53b29edca498795a362ac651547605189feeb
jeisson-gml <jeissonm@gmlsoftware.com>
2020-07-05 15:23:01 -0500

    JM Create react App

---
+++ src/HookApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const HookApp = (props) => {
+  return (
+    <>
+      <h1>Hola Mundo!</h1>
+    </>
+  );
+};
+
+HookApp.propTypes = {};
+
+export default HookApp;
