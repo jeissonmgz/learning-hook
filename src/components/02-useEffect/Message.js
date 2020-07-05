36ecd93a2e62e3dda97f11b4407abd0eaf634cc4
jeisson-gml <jeissonm@gmlsoftware.com>
2020-07-05 16:32:02 -0500

    JM Remove event

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export const Message = () => {
+  const mouseMove = (e) => {
+    const { x, y } = e;
+    console.log(`Point (${x},${y})`);
+  };
+  useEffect(() => {
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
   return (
     <>
       <h3>Genial!</h3>
