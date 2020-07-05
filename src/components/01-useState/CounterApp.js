cba7b1fc37aafb918e75e0579652dda570e2fbba
jeisson-gml <jeissonm@gmlsoftware.com>
2020-07-05 15:42:54 -0500

    JM Hook usestate

---
+++ src/components/01-useState/CounterApp.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./counter.css";
+
+const CounterApp = (props) => {
+  const [state, setstate] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+    counter4: 40,
+  });
+  const { counter1, counter2 } = state;
+  return (
+    <>
+      <h1>Counter1 {counter1}</h1>
+      <h1>Counter2 {counter2}</h1>
+      <br />
+      <button
+        className="btn btn-primary"
+        onClick={() =>
+          setstate({
+            ...state,
+            counter1: counter1 + 1,
+          })
+        }
+      >
+        +1
+      </button>
+    </>
+  );
+};
+
+CounterApp.propTypes = {};
+
+export default CounterApp;
