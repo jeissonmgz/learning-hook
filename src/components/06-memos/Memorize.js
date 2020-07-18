874394259df0409da1372f957400d43c8c13f34e
jeisson-gml <jeissonm@gmlsoftware.com>
2020-07-17 21:50:34 -0500

    JM useMemo

---
+++ src/components/06-memos/Memorize.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+import "../02-useEffect/effect.css";
+import { useCounter } from "../../hooks/useCounter";
+import { Small } from "./Small";
+
+export const Memorize = () => {
+  const { counter, increment } = useCounter(10);
+  const [show, setShow] = useState(true);
+
+  return (
+    <>
+      <h1>
+        Counter: <Small value={counter} />
+      </h1>
+      <hr />
+      <button className="btn btn-primary" onClick={increment}>
+        +1
+      </button>
+      <button
+        className="btn btn-outline-primary ml-3"
+        onClick={() => setShow(!show)}
+      >
+        Show / Hide {JSON.stringify(show)}
+      </button>
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Layout from "./components/05-useLayoutEffect/Layout";
+import { Memorize } from "./components/06-memos/Memorize";
 
-ReactDOM.render(<Layout />, document.getElementById("root"));
+ReactDOM.render(<Memorize />, document.getElementById("root"));
