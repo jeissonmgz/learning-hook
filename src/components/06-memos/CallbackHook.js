7a022cfbf458ce90804ba8367ae9cd5cf2308584
jeisson-gml <jeissonm@gmlsoftware.com>
2020-07-17 22:15:34 -0500

    JM useCallback

---
+++ src/components/06-memos/CallbackHook.js
@@ -0,0 +1,21 @@
+import React, { useState, useCallback } from "react";
+
+import "./../02-useEffect/effect.css";
+import { ShowIncrement } from "./ShowIncrement";
+
+export const CallbackHook = () => {
+  const [counter, setCounter] = useState(10);
+  const incremet = useCallback(
+    (num) => {
+      setCounter((counter) => counter + num);
+    },
+    [setCounter]
+  );
+  return (
+    <>
+      <h1>Use Callback Hooks: {counter}</h1>
+      <hr />
+      <ShowIncrement increment={incremet} />
+    </>
+  );
+};
