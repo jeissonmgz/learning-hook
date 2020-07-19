413f25f626d6dafd32b876f365467e1b94ebe77f
jeisson-gml <jeissonm@gmlsoftware.com>
2020-07-19 18:01:59 -0500

    JM Implement context

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { AppRouter } from "./AppRouter";
+import { UserContext } from "./UserContext";
 
 export const MainApp = () => {
+  const user = {
+    id: 123,
+    name: "Jase",
+    email: "jeissonmgz@gmail.com",
+  };
   return (
-    <>
+    <UserContext.Provider value={user}>
       <AppRouter />
-    </>
+    </UserContext.Provider>
   );
 };
